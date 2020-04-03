a42b43cd256a4e4b004212b26cafc2269ab8bcd8
GabrielVNascimento <gabrielnascimento.udd@gmail.com>
2020-04-03 17:56:23 -0300

    AV1 de desenvolvimento de aplicações movéis baseada  na semana omnistack

---
+++ backend/src/Controllers/OngController.js
@@ -0,0 +1,36 @@
+const generateUniqueId = require('../Utils/generateUniqueId');
+const connection = require('../database/connection');
+
+
+
+module.exports ={
+
+
+    async index(request, response) {
+    
+      const ongs = await connection('ongs').select('*');
+    
+      return response.json(ongs);
+    
+    },
+
+    async create(request, response){
+
+        const {name, email, whatsapp, cidade, uf} = request.body;
+
+        const id = generateUniqueId()
+        
+        await connection('ongs').insert({
+            id,
+            name,
+            email,
+            whatsapp,
+            cidade,
+            uf,
+    
+    })
+    
+    return response.json({ id });
+
+}
+};
